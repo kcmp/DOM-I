a148ce84056d3f35ef6d94d3204bd973a0bf5af7
unknown <kcmp711@gmail.com>
2019-01-30 22:42:44 -0500

    completed mvp and part of stretch

--- js/index.js
+++ js/index.js
@@ -78,7 +78,50 @@ let button = document.querySelector("button");
 button.textContent = 'Get Started';
 // cta.innerHTML = siteContent['cta']['h1'].replace(/\s/g,"</br>");
 
+//main-content
+//headers h4
+let header = document.querySelectorAll('h4');
+// header.style.color = 'blue';
 
+const headerColor = document.querySelectorAll('.main-content h4');
+for (let i = 0; i < headerColor.length; i++){
+const headerColors= headerColor[i];
+headerColors.style.color = "blue";
+headerColors.style.background = "yellow";
+headerColors.style.width="20%"
+}
 
+header[0].textContent = siteContent["main-content"]['features-h4'];
+header[1].textContent = siteContent["main-content"]['about-h4'];
+header[2].textContent = siteContent["main-content"]['services-h4'];
+header[3].textContent = siteContent["main-content"]['product-h4'];
+header[4].textContent = siteContent["main-content"]['vision-h4'];
 
+//content p
+let content = document.querySelectorAll('p');
+content[0].textContent = siteContent["main-content"]['features-content'];
+content[1].textContent = siteContent["main-content"]['about-content'];
+content[2].textContent = siteContent["main-content"]['services-content'];
+content[3].textContent = siteContent["main-content"]['product-content'];
+content[4].textContent = siteContent["main-content"]['vision-content'];
 
+let middleImage = document.getElementById("middle-img");
+middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+//contact
+//headers h4
+
+header[5].textContent = siteContent.contact['contact-h4'];
+
+//content p
+
+content[5].textContent = siteContent.contact.address;
+content[6].textContent = siteContent.contact.phone;
+content[7].textContent = siteContent.contact.email;
+
+let addressOne = document.querySelector('.contact p')
+addressOne.style.width="25%"
+
+//footer
+
+content[8].textContent = siteContent.footer.copyright; 
